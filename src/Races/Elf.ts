451577ab7cefbc7b54fde59c852fb0f7ca520322
leovcampos <leonardoviniciuscampos@gmail.com>
2022-11-18 16:59:26 -0300

    req02

---
+++ src/Races/Elf.ts
@@ -0,0 +1,22 @@
+import Race from './Race';
+
+class Elf extends Race {
+  private _maxLifePoints: number;
+  static _countInstancesCreated = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 99;
+    Elf._countInstancesCreated += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;   
+  }
+
+  static createdRacesInstances(): number {
+    return this._countInstancesCreated;
+  }
+}
+
+export default Elf;
