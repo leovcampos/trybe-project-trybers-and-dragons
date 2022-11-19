326dd5d7aa5ed879fe60b4026a83edbda1c03e6a
leovcampos <leonardoviniciuscampos@gmail.com>
2022-11-18 22:25:36 -0300

    req09

---
+++ src/Monster.ts
@@ -0,0 +1,29 @@
+import { SimpleFighter } from './Fighter';
+
+class Monster implements SimpleFighter {
+  constructor(
+    private _lifePoints: number = 85,
+    private _strength: number = 63,
+  ) { }
+
+  receiveDamage(attackPoints: number): number {
+    this._lifePoints -= attackPoints;
+    if (this._lifePoints <= 0) this._lifePoints = -1;
+
+    return this._lifePoints;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+}
+
+export default Monster;
