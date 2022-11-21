9048cb5a1fea4ae9eb0955cdda7618b5a5361704
leovcampos <leonardoviniciuscampos@gmail.com>
2022-11-21 13:17:57 -0300

    req12

--- src/Dragon.ts
+++ src/Dragon.ts
@@ -1,7 +1,15 @@
 import Monster from './Monster';
 
 class Dragon extends Monster {
-    
+  constructor(
+    private _name: string = 'Dragon',
+  ) {
+    super(999, 88);
+  }
+
+  get name(): string {
+    return this._name;
+  }
 }
 
 export default Dragon;
